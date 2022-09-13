db03f23810c7cc39cc53311c4047cbabce001034
Ali Shouman <alishouman010@gmail.com>
2022-09-13 16:51:44 +0300

    frist-testing

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,1 @@
-const sign_up_email=document.querySelector("#sign_up_email");
-const join_form=document.querySelector("#join-form");
-const signup=document.querySelector("#signup-form");
-const close_create=document.querySelector("#close_create");
-
-sign_up_email.addEventListener('click',()=>{
-  join_form.classList.add('goBack');
-  signup.classList.remove('d-none');
-  
-});
-
-close_create.addEventListener('click',()=>{
-  join_form.classList.remove('goBack');
-  signup.classList.add('d-none');
-  
-});
+ 
