ab8037faf305ba5173fcba0659cb87e64b8c97e1
Ali Shouman <alishouman010@gmail.com>
2022-09-14 22:22:18 +0300

    edit profile

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,39 @@
- 
+//  tags
+const tweets_btn = document.getElementById("tweets_btn");
+const media_btn = document.getElementById("media_btn");
+const likes_btn = document.getElementById("liked_btn");
+//container tweet
+const usertweet = document.getElementById("tweet");
+const liked_tweet = document.getElementById("liked_tweet");
+const media_tweet = document.getElementById("media_tweet");
+
+tweets_btn.addEventListener("click", () => {
+  // remove active class and put reomve from screen
+  removeActiveClass();
+  tweets_btn.classList.add("active");
+  usertweet.classList.remove("d-none");
+});
+
+media_btn.addEventListener("click", () => {
+  // remove active class and put reomve from screen
+  removeActiveClass();
+  media_btn.classList.add("active");
+  media_tweet.classList.remove("d-none");
+});
+
+likes_btn.addEventListener("click", () => {
+  // remove active class and put reomve from screen
+  removeActiveClass();
+  likes_btn.classList.add("active");
+  liked_tweet.classList.remove("d-none");
+});
+
+function removeActiveClass() {
+  tweets_btn.classList.remove("active");
+  media_btn.classList.remove("active");
+  likes_btn.classList.remove("active");
+
+  usertweet.classList.add("d-none");
+  liked_tweet.classList.add("d-none");
+  media_tweet.classList.add("d-none");
+}
