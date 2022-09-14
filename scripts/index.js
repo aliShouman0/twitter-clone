6e7da3f2c418602511ffbf56a62eb05851486d2e
Ali Shouman <alishouman010@gmail.com>
2022-09-15 00:47:24 +0300

    edit profile js

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,28 @@ const likes_btn = document.getElementById("liked_btn");
 const usertweet = document.getElementById("tweet");
 const liked_tweet = document.getElementById("liked_tweet");
 const media_tweet = document.getElementById("media_tweet");
+const edit_container = document.getElementById("edit_container");
+const overlay = document.getElementById("overlay");
+const close_edit_profile = document.getElementById("close");
+const edit_profile = document.getElementById("edit_profile");
+const body = document.getElementById("body");
 
+// edit profile
+edit_profile.addEventListener("click", () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  overlay.classList.remove("d-none");
+  edit_container.classList.remove("d-none");
+  body.classList.add("overflow-n");
+});
+
+close_edit_profile.addEventListener("click", () => {
+  overlay.classList.add("d-none");
+  edit_container.classList.add("d-none");
+  body.classList.remove("overflow-n");
+});
+
+// for bar of 3 btn
 tweets_btn.addEventListener("click", () => {
   // remove active class and put reomve from screen
   removeActiveClass();
