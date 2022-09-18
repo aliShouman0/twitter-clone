ceeac53ed1e5001ce5a1af7ad497a950024637ec
Ali Shouman <alishouman010@gmail.com>
2022-09-18 14:10:12 +0300

    folow api

---
+++ follow.php
@@ -0,0 +1,24 @@
+<?php
+include("connection.php");
+$done = false;
+
+if (
+  isset($_POST["user_id"]) && isset($_POST["follow_user_id"])
+
+) {
+  $user_id = $_POST["user_id"];
+  $follow_user_id = $_POST["follow_user_id"];
+
+  $query = $mysqli->prepare("INSERT INTO follows (user_id,follow_user_id) 
+        VALUES(?,?)");
+  $query->bind_param("ii", $user_id, $follow_user_id);
+
+  if ($query->execute()) {
+    $done = true;
+  }
+}
+
+$result = [
+  "done" => $done
+];
+echo json_encode($result);
--- getUnFollowUser.php
+++ getUnFollowUser.php
@@ -17,7 +17,6 @@ if (
       $userInfo[] = $row;
     }
     $done = true;
-    $done = true;
   }
 }
 
